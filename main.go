1a3a60f53595d9871d4c3b4b68c4426e55221595
durban.zhang <durban.zhang@gmail.com>
2017-05-10 10:12:38 +0800

    add upload file

--- main.go
+++ main.go
@@ -7,6 +7,7 @@ import (
 	"io"
 	"log"
 	"net/http"
+	"os"
 	"strconv"
 	"strings"
 	"time"
@@ -80,9 +81,43 @@ func login(w http.ResponseWriter, r *http.Request) {
 
 }
 
+func upload(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("Method:", r.Method)
+	if r.Method == "POST" {
+		r.ParseMultipartForm(32 << 20)
+		file, handler, err := r.FormFile("uploadfile")
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+
+		defer file.Close()
+		fmt.Fprint(w, "%v", handler.Header)
+
+		f, err := os.OpenFile("./static/tmp/"+handler.Filename, os.O_WRONLY|os.O_CREATE, 0666) // 此处假设当前目录下已存在./static/tmp/目录
+
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+		defer f.Close()
+		io.Copy(f, file)
+
+	} else if r.Method == "GET" {
+		crutime := time.Now().Unix()
+		h := md5.New()
+		io.WriteString(h, strconv.FormatInt(crutime, 10))
+		token := fmt.Sprintf("%x", h.Sum(nil))
+
+		t, _ := template.ParseFiles("views/upload.gtpl")
+		t.Execute(w, token)
+	}
+}
+
 func main() {
 	http.HandleFunc("/", sayHello)
 	http.HandleFunc("/login", login)
+	http.HandleFunc("/upload", upload)
 	err := http.ListenAndServe(":9999", nil)
 	if err != nil {
 		log.Fatal("ListenAndServe:", err)
