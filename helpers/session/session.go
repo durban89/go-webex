1678c97507b76a919b051afea979b49dd9fee1d4
durban.zhang <durban.zhang@gmail.com>
2017-05-16 18:19:00 +0800

    update session

--- helpers/session/session.go
+++ helpers/session/session.go
@@ -12,6 +12,18 @@ import (
 
 var provides = make(map[string]Provider)
 
+func Register(name string, provider Provider) {
+	if provider == nil {
+		panic("session: Register provide is nil")
+	}
+
+	if _, dup := provides[name]; dup {
+		panic("session: Register called twice for provide " + name)
+	}
+
+	provides[name] = provider
+}
+
 type Manager struct {
 	cookieName  string
 	lock        sync.Mutex
